0d2b437c884482f2ffa629183ab35d353309483b
Vishal Kumar Pandey <vishal@Vishals-MacBook-Air.local>
2018-03-14 11:42:53 +0530

    toast removed

--- app/src/main/java/com/example/vishal/gittest/MainActivity.java
+++ app/src/main/java/com/example/vishal/gittest/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "A new line is added", Toast.LENGTH_SHORT).show();
     }
 }
