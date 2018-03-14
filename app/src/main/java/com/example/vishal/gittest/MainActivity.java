a9c526876c4e4b870a89b8a0be14717059b530fe
Vishal Kumar Pandey <vishal@Vishals-MacBook-Air.local>
2018-03-14 11:55:45 +0530

    user 2 added toast

--- app/src/main/java/com/example/vishal/gittest/MainActivity.java
+++ app/src/main/java/com/example/vishal/gittest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "new line is added by user2", Toast.LENGTH_SHORT).show();
     }
 }
