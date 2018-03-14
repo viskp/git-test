b867a1c1e819de246a9bbe9e0f26968ca0291b4d
Vishal Kumar Pandey <vishal@Vishals-MacBook-Air.local>
2018-03-14 14:29:24 +0530

    new line

--- app/src/main/java/com/example/vishal/gittest/MainActivity.java
+++ app/src/main/java/com/example/vishal/gittest/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(newTest);
         setContentView(R.layout.activity_main);
         //comment from user1
+        //comment added for new branch "test-branch"
+        //new line added in branch "master"
     }
 }
